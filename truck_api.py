cb2646314ad3fd91b1024b8cec29c7a20875087d
morinarobert <morina.robert14@gmail.com>
2019-12-11 15:41:32 -0500

    Update truck_api.py

--- truck_api.py
+++ truck_api.py
@@ -1,12 +1,14 @@
 import requests
 from pprint import pprint
+import json
 
-locationUrlFromLatLong = "https://developers.zomato.com/api/v2.1/cities?lat=28&lon=77"
+url = "https://developers.zomato.com/api/v2.1/restaurant?res_id=19160430"
 header = {"User-agent": "curl/7.43.0", "Accept": "application/json", "user_key": "c9fe3366cf8c52aa35a3afe563b41234"}
 
-response = requests.get(locationUrlFromLatLong, headers=header)
+response = requests.get(url, headers=header)
+json_body = response.json()
 
-pprint(response.json())
+print(json_body["photos"][0]["photo"]["url"])
 
 
 
